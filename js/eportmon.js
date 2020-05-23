7962f0a0226fe3c76bc1e07b0a55e0d877353c0e
SANDIP CHITALE <sandipchitale@gmail.com>
2020-05-24 00:13:13 +0100

    New icon. Toggle monitoring.

--- js/eportmon.js
+++ js/eportmon.js
@@ -2,20 +2,94 @@
   const process = require('process');
   const spawn = require('child_process').spawn;
   const remote = require('remote');
+  const path = require('path');
+  const pathExtra = require('path-extra');
+  const fileSystem = require('fs');
 
   angular
   .module('EportmonApp', [])
-  .controller('EportmonCtrl', ['$scope', '$window', '$log', function($scope, $window, $log){
+  .controller('EportmonCtrl', ['$scope', '$window', '$log', '$interval',function($scope, $window, $log, $interval){
     var vm = this;
     vm.wait = false;
 
+
+    var intervalId;
+    vm.monitor = true;
+    vm.toggleMonitoring = function() {
+      vm.monitor = !vm.monitor;
+      if (vm.monitor) {
+        vm.doNetstat();
+        startMonitoring();
+      } else {
+        stopMonitoring();
+      }
+    }
+
+    function startMonitoring() {
+      vm.netstat();
+      intervalId = $interval(vm.netstat, 10000);
+    }
+
+    function stopMonitoring() {
+      if (intervalId) {
+        $interval.cancel(intervalId);
+        intervalId = null;
+      }
+    }
+
+    // Utility
+    const isFile = function(path) {
+      try {
+        return fileSystem.statSync(path) && fileSystem.statSync(path).isFile()
+      } catch (e) {
+        return false;
+      }
+    }
+
+    const isDirectory = function(path) {
+      try {
+        return fileSystem.statSync(path) && fileSystem.statSync(path).isDirectory()
+      } catch (e) {
+        return false;
+      }
+    }
+
+    var eportmonSettingsFilePath = path.join(pathExtra.homedir(), '.portmon.json');
+    var eportmonSettings = {
+      ports: '4200,8080,8765,2910'
+    }
+
+    // save settings
+    function saveSettings() {
+      fileSystem.writeFileSync(eportmonSettingsFilePath, JSON.stringify(eportmonSettings, null, '  '));
+    }
+
+    // load settings
+    function loadSettings() {
+      if (isFile(eportmonSettingsFilePath)) {
+          try {
+              eportmonSettings = JSON.parse(fileSystem.readFileSync(eportmonSettingsFilePath));
+          } catch (e) {
+          }
+      } else {
+          saveSettings();
+      }
+    };
+    loadSettings();
+
     vm.listeningOnly = true;
-    vm.onlyPorts = '';
+    vm.onlyPorts = eportmonSettings.ports;
     vm.onlyPortsArray = [];
     vm.ports = [];
+
+    vm.portsChanged = function() {
+      eportmonSettings.ports = vm.onlyPorts;
+      saveSettings();
+    }
+
     vm.filterPorts = function(portLine) {
       return (!vm.listeningOnly || portLine[2] === 'LISTENING') &&
-             (vm.onlyPortsArray.length === 0 || vm.onlyPortsArray.indexOf(portLine[1]) !== -1);
+              (vm.onlyPortsArray.length === 0 || vm.onlyPortsArray.indexOf(portLine[1]) !== -1);
     }
 
     vm.orderByPorts = function(portLine) {
@@ -41,13 +115,14 @@
 
           kp.on('close', (code) => {
             $log.debug('Exit code: ' + code);
-            netstat();
+            vm.netstat();
           });
         }
       }
     }
 
-    function netstat() {
+
+    vm.doNetstat =function () {
       var portLines = '';
 
       vm.onlyPortsArray = vm.onlyPorts.split(/,/);
@@ -73,29 +148,44 @@
         portLines = portLines.split(/\r\n/);
         $scope.$apply(function() {
           if (angular.isArray(portLines) && portLines.length > 5) {
-            portLines.splice(0, 5);
-            if (portLines[portLines.length-1] === '') [
-              portLines.splice(portLines.length-1, 1)
-            ]
-            angular.forEach(portLines, function(portLine, index, portLines) {
-              portLines[index] = portLine.split(/\s+/);
-              portLines[index].splice(3,1);
-              portLines[index].splice(0,1);
-              portLines[index][1] = portLines[index][1].split(/:/);
-              portLines[index][1].splice(0,1);
-              portLines[index][1] = portLines[index][1][0];
-            });
-            vm.ports = portLines;
+            try {
+              portLines.splice(0, 5);
+              if (portLines[portLines.length-1] === '') [
+                portLines.splice(portLines.length-1, 1)
+              ]
+              angular.forEach(portLines, function(portLine, index, portLines) {
+                portLines[index] = portLine.split(/\s+/);
+                portLines[index].splice(3,1);
+                portLines[index].splice(0,1);
+                portLines[index][1] = portLines[index][1].split(/:/);
+                portLines[index][1].splice(0,1);
+                portLines[index][1] = portLines[index][1][0];
+              });
+              if (vm.listeningOnly) {
+                portLines.filter(function(portLine) {
+                  return portLine[2]  === 'LISTENING';
+                });
+              }
+              vm.ports = portLines;
+            } finally {
+
+            }
           }
         });
       });
     }
-    vm.netstat = netstat;
+
+    vm.netstat = function () {
+      if (vm.monitor) {
+        vm.doNetstat();
+      }
+    }
 
     vm.quit = function() {
       remote.getCurrentWindow().close();
     };
 
-    netstat();
+    startMonitoring();
+
   }]);
 })();
